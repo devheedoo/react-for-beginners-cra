7a391e72ec726ca11e3187200ad79aa3e8898fd3
Choco Kim <devheedoo@gmail.com>
2021-12-05 21:54:33 +0900

    problem: Render whenever state changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleClick = () => setCounter((prev) => prev + 1);
+  console.log('rendered');
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Hello, Button!'} />
+      <h1>{counter}</h1>
+      <button onClick={handleClick}>click me!</button>
     </div>
   );
 }
