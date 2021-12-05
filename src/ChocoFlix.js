cb96c387b3d1c04db7478a9bb7ae401d7d3e84a1
Choco Kim <devheedoo@gmail.com>
2021-12-06 04:15:43 +0900

    feat(ChocoFlix): Divide Movie component

--- src/ChocoFlix.js
+++ src/ChocoFlix.js
@@ -1,45 +1,5 @@
-import { useEffect, useState } from 'react';
-
-const MOVIE_SOURCE_URL =
-  'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year';
-
 function ChocoFlix() {
-  const [isLoading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (await fetch(MOVIE_SOURCE_URL)).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      <h1>CHOCOFLIX</h1>
-      {isLoading ? (
-        <strong>loading...</strong>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default ChocoFlix;
---
+++ src/routes/Home.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+
+const MOVIE_SOURCE_URL =
+  'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year';
+
+function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (await fetch(MOVIE_SOURCE_URL)).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>CHOCOFLIX</h1>
+      {isLoading ? (
+        <strong>loading...</strong>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              posterImage={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
