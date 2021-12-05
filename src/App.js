c09563c2f789ddee35077b0d0bc5b2e2c4117985
Choco Kim <devheedoo@gmail.com>
2021-12-05 22:21:51 +0900

    feat: Test useEffect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const handleClick = () => setCounter((prev) => prev + 1);
-  console.log('called every render');
-  useEffect(() => console.log('called only once'), []);
+  const handleChange = (event) => setKeyword(event.target.value);
+  console.log('I run every time.');
+  useEffect(() => console.log('I run only once'), []);
+  useEffect(() => console.log('I run when "counter" changes.'), [counter]);
+  useEffect(() => console.log('I run when "keyword" changes.'), [keyword]);
+  useEffect(
+    () => console.log('I run when "counter" and "keyword" changes.'),
+    [counter, keyword]
+  );
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={handleChange}
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={handleClick}>click me!</button>
     </div>
