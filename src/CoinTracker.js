9d693ff550b86fdcb2a42e2ba5bf46389c00b374
Choco Kim <devheedoo@gmail.com>
2021-12-06 03:13:11 +0900

    feat(CoinTracker): Load coins and hide loading

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const COINS_URL = 'https://api.coinpaprika.com/v1/tickers';
+
 function CoinTracker() {
-  return <></>;
+  const [isLoading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch(COINS_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>The Coins!</h1>
+      {isLoading ? <strong>Loading...</strong> : null}
+    </div>
+  );
 }
 
 export default CoinTracker;
