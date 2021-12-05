b002af393e0452e9f1210952bd41f7996890496f
Choco Kim <devheedoo@gmail.com>
2021-12-06 02:23:54 +0900

    feat: Apply react cleanup function in useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
+
+function Hello() {
+  useEffect(() => {
+    console.log('Hello component is created.');
+    return () => console.log('Hello component is destroyed.');
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const handleClick = () => setShowing((curr) => !curr);
   return (
     <div>
-      <button onClick={handleClick}>click me!</button>
+      <button onClick={handleClick}>{showing ? 'Hide' : 'Show'}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
